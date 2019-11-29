3a40f38a102cd115e441c815c2aaaaaef92452d6
justsomeguy <thenaders3@gmail.com>
2019-11-28 16:22:12 -0800

    added truffle to project

--- contracts/KingContract.sol
+++ contracts/KingContract.sol
@@ -1,24 +1,27 @@
 pragma solidity ^0.5.0;
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract KingDappContract {
+    using SafeMath for uint;
+
     address payable public King;
-    uint public kingRansom;
+    uint256 public kingRansom;
 
     constructor() public payable {
         King = msg.sender;
         kingRansom = msg.value;
     }
 
-    event Coronation(address indexed _newKing, uint _kingRansom);
+    event Coronation(address indexed _newKing, uint256 _kingRansom);
 
     function() external payable {
-        revert("Please invoke a funtion when sending Eth to this address");
+        revert();
     }
 
     function becomeKing() public payable {
-        require(msg.value >= (kingRansom + 0.1 ether), "Eth included was not enough");
+        require(msg.value >= (kingRansom.add(0.1 ether)));
         address payable oldKing = King;
-        uint oldRansom = kingRansom;
+        uint256 oldRansom = kingRansom;
         King = msg.sender;
         kingRansom = msg.value;
         oldKing.transfer(oldRansom);
